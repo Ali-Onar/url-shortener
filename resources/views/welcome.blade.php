47ff7b3e2545a107068c1c09102299cc1105ffe2
= <alitunacanonar@icloud.com>
2021-05-04 14:56:44 +0300

    the first files are uploaded. Breeze is established. Database connection has been made.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-gayout>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 flex justify-center bg-white border-b border-gray-200">
+                    Welcome
+                </div>
+            </div>
+        </div>
+    </div>
+</x-gayout>
